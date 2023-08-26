0f53b2a2c213c451d84fc7a569c1e3d123b638fd
anthoniBortolotto <antho@outlook.com.br>
2023-08-26 15:46:32 -0300

    ilumination fixed

--- movement.py
+++ movement.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy as np
+
 valor = 'images/shot00'
 
 # valor nulo para comparar para saber se é a primeira imagem
@@ -16,31 +16,29 @@ for i in range(3, 16):
     gray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
     if (antigo is not None):
         diferenca = cv2.absdiff(gray, antigo)
+        #corrige mudanças de iluminação em todas as imagens
+        diferenca = cv2.threshold(diferenca, 110, 255, cv2.THRESH_BINARY)[1] 
         
-        # corrige mudanças de iluminação nas imagens
-        ilumination_change = cv2.threshold(diferenca, 25, 255, cv2.THRESH_BINARY)[1]
+        # corrige mudanças de iluminação em apenas algumas imagens
+        """ ilumination_change = cv2.threshold(diferenca, 30, 255, cv2.THRESH_BINARY)[1]
         num = cv2.countNonZero(ilumination_change) / (gray.size / 100)
+
         if(num > 10):
-            media = cv2.mean(diferenca)[0]
+            #tenta corrigir a iluminação
+            #torna preto todos os pixels maiores que 110
+            diferenca = cv2.threshold(diferenca, 110, 255, cv2.THRESH_BINARY)[1] """
 
-
-            matriz_resultante = np.maximum(gray * 1.8, 0).astype(np.uint8)
-            #matriz_resultante = np.minimum(gray / 0.53, 255).astype(np.uint8)
-            diferenca = cv2.absdiff(matriz_resultante , antigo)
-
-            cv2.imshow('Imagem', diferenca)
+            
         
         
         #print(diferenca)
-        #cv2.imshow('Imagem', diferenca)
+        cv2.imshow('Imagem', diferenca)
         cv2.waitKey(0)
     antigo = gray
-# Verificar se a imagem foi carregada corretamente
+    # Verificar se a imagem foi carregada corretamente
     if imagem is None:
         print("Erro ao carregar a imagem")
     else:
-    # Exibir a imagem em uma janela
-        #print(imagem)
         cv2.waitKey(0)  # Esperar até uma tecla ser pressionada
         cv2.destroyAllWindows()  # Fechar todas as janelas abertas
 
